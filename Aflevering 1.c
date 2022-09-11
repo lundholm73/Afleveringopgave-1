c4d5fdd2d7505db827b7dcf37338dcc99aebb5ad
Christian Lundholm <clso22@student.aau.dk>
2022-09-11 14:52:48 +0200

    Oprettelse

---
+++ Aflevering 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int sekunder = 0;
+
+    int main (void)
+    {
+        printf("Input sekunder");
+        scanf("%lf", sekunder)
+        printf("%fl",sekunder)
+    }
+
+        return 0;
